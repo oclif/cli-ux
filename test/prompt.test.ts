93a182d71f13b7291f090878ec148ae4932f8642
Thomas Dvornik <amphro@gmail.com>
2018-04-21 15:51:30 -0600

    feat: allow the user to require no input, which allows for defaults (#27)
    * feat: allow the user to require no input, which allows for defaults
    
    * fix: passing tests

---
+++ test/prompt.test.ts
@@ -0,0 +1,30 @@
+import cli from '../src'
+
+import {expect, fancy} from './fancy'
+
+describe('prompt', () => {
+  fancy
+  .stdout()
+  .stderr()
+  .end('requires input', async () => {
+    const promptPromise = cli.prompt('Require input?')
+    process.stdin.emit('data', '')
+    process.stdin.emit('data', 'answer')
+    const answer = await promptPromise
+    await cli.done()
+    expect(answer).to.equal('answer')
+  })
+
+  fancy
+  .stdout()
+  .stderr()
+  .end('does not require input', async () => {
+      const promptPromise = cli.prompt('Require input?', {
+        required: false
+      })
+      process.stdin.emit('data', '')
+      const answer = await promptPromise
+      await cli.done()
+      expect(answer).to.equal('')
+  })
+})
