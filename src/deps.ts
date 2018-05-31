7ddf325c242b5fec2db4b212cea09f72b496176c
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-05-31 15:42:14 -0700

    fix: typescript 2.9 (#33)

--- src/deps.ts
+++ src/deps.ts
@@ -1,27 +1,16 @@
-import screen = require('@oclif/screen')
-import ansiStyles = require('ansi-styles')
-import stripAnsi = require('strip-ansi')
-
-import open = require('./open')
-import prompt = require('./prompt')
-import styledHeader = require('./styled/header')
-import styledJSON = require('./styled/json')
-import styledObject = require('./styled/object')
-import table = require('./styled/table')
-
 export const deps = {
-  get stripAnsi(): typeof stripAnsi { return fetch('strip-ansi') },
-  get ansiStyles(): typeof ansiStyles { return fetch('ansi-styles') },
+  get stripAnsi(): typeof import ('strip-ansi') { return fetch('strip-ansi') },
+  get ansiStyles(): typeof import ('ansi-styles') { return fetch('ansi-styles') },
   get ansiEscapes(): any { return fetch('ansi-escapes') },
   get passwordPrompt(): any { return fetch('password-prompt') },
-  get screen(): typeof screen { return fetch('@oclif/screen') },
+  get screen(): typeof import ('@oclif/screen') { return fetch('@oclif/screen') },
 
-  get open(): typeof open.default { return fetch('./open').default },
-  get prompt(): typeof prompt.default { return fetch('./prompt').default },
-  get styledObject(): typeof styledObject.default { return fetch('./styled/object').default },
-  get styledHeader(): typeof styledHeader.default { return fetch('./styled/header').default },
-  get styledJSON(): typeof styledJSON.default { return fetch('./styled/json').default },
-  get table(): typeof table.default { return fetch('./styled/table').default },
+  get open(): typeof import ('./open').default { return fetch('./open').default },
+  get prompt(): typeof import ('./prompt').default { return fetch('./prompt').default },
+  get styledObject(): typeof import ('./styled/object').default { return fetch('./styled/object').default },
+  get styledHeader(): typeof import ('./styled/header').default { return fetch('./styled/header').default },
+  get styledJSON(): typeof import ('./styled/json').default { return fetch('./styled/json').default },
+  get table(): typeof import ('./styled/table').default { return fetch('./styled/table').default },
 }
 
 const cache: any = {}
