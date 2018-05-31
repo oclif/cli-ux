f9d756f55ce3120e62f9c0fcdc5e1efa2ca1fddc
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2018-05-31 15:49:00 -0700

    fix lint

--- src/deps.ts
+++ src/deps.ts
@@ -1,17 +1,17 @@
 export const deps = {
   get stripAnsi(): typeof import ('strip-ansi') { return fetch('strip-ansi') },
-  get ansiStyles(): typeof import('ansi-styles') { return fetch('ansi-styles') },
+  get ansiStyles(): typeof import ('ansi-styles') { return fetch('ansi-styles') },
   get ansiEscapes(): any { return fetch('ansi-escapes') },
   get passwordPrompt(): any { return fetch('password-prompt') },
-  get screen(): typeof import('@oclif/screen') { return fetch('@oclif/screen') },
+  get screen(): typeof import ('@oclif/screen') { return fetch('@oclif/screen') },
 
-  get open(): typeof import('./open').default { return fetch('./open').default },
-  get prompt(): typeof import('./prompt').default { return fetch('./prompt').default },
-  get styledObject(): typeof import('./styled/object').default { return fetch('./styled/object').default },
-  get styledHeader(): typeof import('./styled/header').default { return fetch('./styled/header').default },
-  get styledJSON(): typeof import('./styled/json').default { return fetch('./styled/json').default },
-  get table(): typeof import('./styled/table').default { return fetch('./styled/table').default },
-  get wait(): typeof import('./wait').default { return fetch('./wait').default },
+  get open(): typeof import ('./open').default { return fetch('./open').default },
+  get prompt(): typeof import ('./prompt').default { return fetch('./prompt').default },
+  get styledObject(): typeof import ('./styled/object').default { return fetch('./styled/object').default },
+  get styledHeader(): typeof import ('./styled/header').default { return fetch('./styled/header').default },
+  get styledJSON(): typeof import ('./styled/json').default { return fetch('./styled/json').default },
+  get table(): typeof import ('./styled/table').default { return fetch('./styled/table').default },
+  get wait(): typeof import ('./wait').default { return fetch('./wait').default },
 }
 
 const cache: any = {}
