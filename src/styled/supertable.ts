68c5234fc668600b302e7f9f212de836f809fbb7
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2018-09-12 13:35:11 -0700

    edits

--- src/styled/supertable.ts
+++ src/styled/supertable.ts
@@ -6,7 +6,7 @@ import {inspect} from 'util'
 
 const sw = require('string-width')
 
-function outputCSV(data: any[], columns: SuperTable.Column[], options: SuperTable.Options) {
+function outputCSV(data: any[], columns: Column[], options: Options) {
   options.printLine(columns.map(c => c.header).join(','))
   data.forEach((d: any) => {
     let row: string[] = []
@@ -15,23 +15,47 @@ function outputCSV(data: any[], columns: SuperTable.Column[], options: SuperTabl
   })
 }
 
-function outputTable(data: any[], columns: SuperTable.Column[], options: SuperTable.Options) {
-  // find min/max column width
+function outputTable(data: any[], columns: Column[], options: Options) {
+  // find max column width
   for (let col of columns) {
-    if (col.staticWidth) continue
-    const widths = [col.header, ...data.map(row => row[col.key])].map(r => sw(r))
-    col.width = Math.max(...widths) + 1
+    const widths = ['.'.padEnd(col.minWidth!), col.header, ...data.map(row => row[col.key])].map(r => sw(r))
+    col.maxWidth = Math.max(...widths) + 1
+    col.width = col.maxWidth!
   }
 
   const maxWidth = stdtermwidth
   const shouldShorten = () => {
     // don't shorten if full mode
     if (options.full || !process.stdout.isTTY) return
+
     // don't shorten if there is enough screen width
-    if (_.sumBy(columns, c => c.width!) <= maxWidth) return
+    let dataMaxWidth = _.sumBy(columns, c => c.width!)
+    let dataMinWidth = _.sumBy(columns, c => c.minWidth!)
+    let overWidth = dataMaxWidth - maxWidth
+    if (overWidth <= 0) return
+
+    // not enough room, short all cols to minWidth
     for (let col of columns) {
-      if (col.staticWidth || col.width === col.minWidth) continue
-      col.width = col.minWidth || sw(col.header)
+      col.width = col.minWidth //|| sw(col.header)
+    }
+
+    // if sum(minWidth's) is greater, return
+    if (dataMinWidth >= maxWidth) return
+
+    // some wiggle room left, add it back
+    let wiggleRoom = maxWidth - dataMinWidth
+    let needyCols = _.sortBy(columns.map(c => ({key: c.key, needs: c.maxWidth! - c.width!})), c => c.needs)
+    for (let {key, needs} of needyCols) {
+      if (!needs) continue
+      let col = _.find(columns, c => (key === c.key))
+      if (!col) continue
+      if (wiggleRoom > needs) {
+        col.width = col.width! + needs
+        wiggleRoom = wiggleRoom - needs
+      } else if (wiggleRoom) {
+        col.width = col.width! + wiggleRoom
+        wiggleRoom = 0
+      }
     }
   }
   shouldShorten()
@@ -41,7 +65,7 @@ function outputTable(data: any[], columns: SuperTable.Column[], options: SuperTa
     let headers = ''
     for (let col of columns) {
       let header = col.header!
-      headers += header.padEnd(col.width! + 1)
+      headers += header.padEnd(col.width!)
     }
     options.printLine(chalk.bold(headers))
   }
@@ -52,21 +76,20 @@ function outputTable(data: any[], columns: SuperTable.Column[], options: SuperTa
       const width = col.width!
       let o = row[col.key].padEnd(width)
       if (o.length > width) {
-        o = o.slice(0, width - 1) + '…'
+        o = o.slice(0, width - 2) + '… '
       }
-      process.stdout.write(o + ' ')
+      process.stdout.write(o)
     }
     process.stdout.write('\n')
   }
 }
 
-function outputArray(data: any[], columns: SuperTable.Column[], options: SuperTable.Options) {
+function outputArray(data: any[], columns: Column[], options: Options) {
   // build table rows from input array data
   let rows = data.map(d => {
     let row = {}
     for (let col of columns) {
-      let val = _.get(d, col.key)
-      val = col.get && col.get(val, d)
+      let val = col.get(d)
       if (typeof (val) === 'boolean' || !val) val = inspect(val, {breakLength: Infinity})
       _.set(row, col.key, val)
     }
@@ -119,31 +142,32 @@ function outputArray(data: any[], columns: SuperTable.Column[], options: SuperTa
 }
 
 const flags = {
-  columns: Flags.string({exclusive: ['all'], description: 'only show provided columns'}),
+  columns: Flags.string({exclusive: ['all'], description: 'only show provided columns (comma-seperated)'}),
   sort: Flags.string({description: 'property to sort by'}),
-  filter: Flags.string({description: 'filter property by regex'}),
+  filter: Flags.string({description: 'filter property by partial string matching, ex: name=foo'}),
   csv: Flags.boolean({exclusive: ['json'], description: 'output is csv format'}),
-  extended: Flags.boolean({description: 'show more/extended properties'}),
-  full: Flags.boolean({description: 'do not truncate output to fit screen'}),
-  'no-header': Flags.boolean({exclusive: ['csv'], description: 'hide header from output'}),
+  additional: Flags.boolean({description: 'show additional properties'}),
+  'no-truncate': Flags.boolean({description: 'do not truncate output to fit screen'}),
+  'no-header': Flags.boolean({exclusive: ['csv'], description: 'hide table header from output'}),
 }
 
-function build(data: any, cols: SuperTable.ColumnOptions[], options: SuperTable.Options) {
+function display(data: any, cols: SuperTable.Columns, options: SuperTable.Options) {
   // tslint:disable-next-line:no-console
   if (!options.printLine) options.printLine = (s: any) => console.log(s)
   if (!options.sort) options.sort = 'name'
 
   // clean up columns array
-  let columns: SuperTable.Column[] = cols.map(col => {
-    let header = col.header || _.capitalize(col.key.replace(/\_/g, ' '))
-    if (!col.width) col.width = 0
-    if (col.width) col.staticWidth = true
-    if (col.minWidth) col.minWidth = Math.max(col.minWidth, sw(header))
+  let columns: Column[] = Object.keys(cols).map((key: string) => {
+    const col = cols[key]
+    const header = col.header || _.capitalize(key.replace(/\_/g, ' '))
+    const minWidth = Math.max(col.minWidth || 0, sw(header) + 1)
     return {
+      key,
       extended: false,
-      get: (cell: any) => cell,
+      get: (row: any) => row[key],
       ...col,
       header,
+      minWidth,
     }
   })
 
@@ -162,16 +186,14 @@ function build(data: any, cols: SuperTable.ColumnOptions[], options: SuperTable.
 }
 
 export namespace SuperTable {
-  export type ColumnOptions = Partial<Column> & { key: string }
+  export type Columns = {[key: string]: Partial<Column>}
 
   export interface Column {
     key: string
     header: string
-    extended?: boolean
-    minWidth?: number
-    width?: number
-    staticWidth?: boolean
-    get(cell: any, row: any): string
+    extended: boolean
+    minWidth: number
+    get(row: any): string
   }
 
   export interface Options {
@@ -179,14 +201,17 @@ export namespace SuperTable {
     sort?: string,
     filter?: string,
     columns?: string,
-    extended?: boolean,
-    full?: boolean,
+    additional?: boolean,
+    'no-truncate'?: boolean,
     csv?: boolean,
     'no-header'?: boolean,
   }
 }
 
+type Column = SuperTable.Column & {width?: number, maxWidth?: number}
+type Options = SuperTable.Options
+
 export default {
   flags,
-  build,
+  display,
 }
