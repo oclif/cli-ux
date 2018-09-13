ed4038f08ca7bbbc691b82d3fe0c3e95f57fa686
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-09-13 08:41:55 -0700

    fix: ensure ansi-escapes is added
    also updated linter
    
    Fixes https://github.com/oclif/cli-ux/issues/51

--- test/styled/table.test.ts
+++ test/styled/table.test.ts
@@ -4,44 +4,44 @@ import cli from '../../src'
 
 describe('styled/table', () => {
   fancy
-  .stdout()
-  .end('shows a table', output => {
-    cli.table([
+    .stdout()
+    .end('shows a table', output => {
+      cli.table([
       {foo: 1, bar: 1},
       {foo: 2, bar: 2},
       {foo: 3, bar: 3},
-    ], {
-      columns: [
+      ], {
+        columns: [
         {key: 'bar'},
         {key: 'foo'},
-      ]
-    })
-    expect(output.stdout).to.equal(`bar  foo
+        ]
+      })
+      expect(output.stdout).to.equal(`bar  foo
 ───  ───
 1    1
 2    2
 3    3
 `)
-  })
+    })
 
   fancy
-  .stdout()
-  .end('hides the header', output => {
-    cli.table([
+    .stdout()
+    .end('hides the header', output => {
+      cli.table([
       {foo: 1, bar: 1},
       {foo: 2, bar: 2},
       {foo: 3, bar: 3},
-    ], {
-      printHeader: undefined,
-      columns: [
+      ], {
+        printHeader: undefined,
+        columns: [
         {key: 'bar'},
         {key: 'foo'},
-      ]
-    })
+        ]
+      })
 
-    expect(output.stdout).to.equal(`1    1
+      expect(output.stdout).to.equal(`1    1
 2    2
 3    3
 `)
-  })
+    })
 })
