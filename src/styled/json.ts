9c5cf04f9820474958365f186767f2f143d8958d
Mike Donnalley <mdonnalley@salesforce.com>
2020-02-27 09:44:15 -0700

    fix: use util.inspect instead of JSON.stringify

--- src/styled/json.ts
+++ src/styled/json.ts
@@ -1,11 +1,12 @@
 // tslint:disable restrict-plus-operands
 
 import chalk from 'chalk'
+import util from 'util'
 
 import cli from '..'
 
 export default function styledJSON(obj: any) {
-  const json = JSON.stringify(obj, null, 2)
+  const json = util.inspect(obj)
   if (!chalk.enabled) {
     cli.info(json)
     return
