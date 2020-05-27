45c7eb568c5db301a0f90b7cc86678f0a58adefc
Munif Tanjim <muniftanjim@gmail.com>
2020-05-27 12:24:41 +0600

    feat: support multiple filter flags for table

--- src/styled/table.ts
+++ src/styled/table.ts
@@ -58,18 +58,20 @@ class Table<T extends object> {
     })
 
     // filter rows
-    if (this.options.filter) {
-      /* eslint-disable-next-line prefer-const */
-      let [header, regex] = this.options.filter!.split('=')
-      const isNot = header[0] === '-'
-      if (isNot) header = header.substr(1)
-      const col = this.findColumnFromHeader(header)
-      if (!col || !regex) throw new Error('Filter flag has an invalid value')
-      rows = rows.filter((d: any) => {
-        const re = new RegExp(regex)
-        const val = d[col!.key]
-        const match = val.match(re)
-        return isNot ? !match : match
+    if (!_.isEmpty(this.options.filter)) {
+      this.options.filter!.forEach(filter => {
+        /* eslint-disable-next-line prefer-const */
+        let [header, regex] = filter.split('=')
+        const isNot = header[0] === '-'
+        if (isNot) header = header.substr(1)
+        const col = this.findColumnFromHeader(header)
+        if (!col || !regex) throw new Error('Filter flag has an invalid value')
+        rows = rows.filter((d: any) => {
+          const re = new RegExp(regex)
+          const val = d[col!.key]
+          const match = re.test(val)
+          return isNot ? !match : match
+        })
       })
     }
 
@@ -288,7 +290,7 @@ export namespace table {
   export const Flags: {
     columns: F.IOptionFlag<string | undefined>;
     sort: F.IOptionFlag<string | undefined>;
-    filter: F.IOptionFlag<string | undefined>;
+    filter: F.IOptionFlag<string[] | undefined>;
     csv: F.IFlag<boolean>;
     output: F.IOptionFlag<string | undefined>;
     extended: F.IFlag<boolean>;
@@ -297,7 +299,7 @@ export namespace table {
   } = {
     columns: F.string({exclusive: ['extended'], description: 'only show provided columns (comma-separated)'}),
     sort: F.string({description: 'property to sort by (prepend \'-\' for descending)'}),
-    filter: F.string({description: 'filter property by partial string matching, ex: name=foo'}),
+    filter: F.string({description: 'filter property by partial string matching, ex: name=foo', multiple: true}),
     csv: F.boolean({exclusive: ['no-truncate'], description: 'output is csv format [alias: --output=csv]'}),
     output: F.string({
       exclusive: ['no-truncate', 'csv'],
@@ -345,7 +347,7 @@ export namespace table {
   export interface Options {
     [key: string]: any;
     sort?: string;
-    filter?: string;
+    filter?: string[];
     columns?: string;
     extended?: boolean;
     'no-truncate'?: boolean;
