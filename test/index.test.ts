34a0f94f44b97b99a3d09f871989b3f0ec0f4666
Mike Donnalley <mdonnalley@salesforce.com>
2021-12-15 10:40:37 -0700

    chore: fix url hyperlink test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,14 +1,25 @@
 import ux from '../src'
 
 import {expect, fancy} from './fancy'
+// eslint-disable-next-line unicorn/prefer-module
+const hyperlinker = require('hyperlinker')
 
-process.env.FORCE_HYPERLINK = '1'
-
-describe.skip('prompt', () => {
+describe('url', () => {
   fancy
+  .env({FORCE_HYPERLINK: '1'}, {clear: true})
   .stdout()
   .do(() => ux.url('sometext', 'https://google.com'))
   .it('renders hyperlink', async ({stdout}) => {
-    expect(stdout).to.equal('ttps://google.com\u0007sometext\n')
+    expect(stdout).to.equal('sometext\n')
+  })
+})
+
+describe('hyperlinker', () => {
+  fancy
+  .it('renders hyperlink', async () => {
+    const link = hyperlinker('sometext', 'https://google.com', {})
+    // eslint-disable-next-line unicorn/escape-case
+    const expected = '\u001b]8;;https://google.com\u0007sometext\u001b]8;;\u0007'
+    expect(link).to.equal(expected)
   })
 })
